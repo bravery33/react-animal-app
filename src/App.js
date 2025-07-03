878cfefb64b30190ad42ae0e2e2be01526f2252d
bravery33 <diddung83@gmail.com>
2025-07-03 13:21:44 +0900

    feat: support configurable image dimensions

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const jsonLocalStorage = {
 const OPEN_API_DOMAIN = 'https://cataas.com';
 
 const fetchCat = async (text) => {
-  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?json=true`);
+  const response = await fetch(`${OPEN_API_DOMAIN}/cat/says/${text}?width=400&height=400&json=true`);
   const responseJson = await response.json();
   return responseJson.url;
 };
@@ -42,8 +42,8 @@ function App() {
     });
   }
 
-  async function updateMainAnimal() {
-    const newCat = await fetchCat();
+  async function updateMainAnimal(text) {
+    const newCat = await fetchCat(text);
     setMainAnimal(newCat);
     incrementCount();
   }
